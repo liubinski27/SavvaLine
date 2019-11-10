b4d77bd5d374e8255884614109506b0691358094
Konstantin <55506460+liubinski27@users.noreply.github.com>
2019-11-10 19:12:35 +0300

    menu hidden on click

--- src/js/base.js
+++ src/js/base.js
@@ -1,1 +1,29 @@
-console.log('Привет')
+var buttonS = document.getElementById('menu__btn');
+
+var menu = document.getElementById('hamburger-menu');
+
+buttonS.addEventListener('click', myFun);
+
+function myFun() {
+    if (menu.classList.contains('hidden')) {
+        menu.classList.remove('hidden');
+    }
+    else {
+        menu.classList.add('hidden');
+    }
+}
+
+
+
+
+
+
+
+
+function addHidden() {
+    div.classList.add('hidden');
+}
+
+function removeHidden() {
+    div.classList.remove('hidden');
+}
