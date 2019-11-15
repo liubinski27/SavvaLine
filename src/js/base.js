6ac320705e5432d80131c2ab001f6a1c5b6f9c47
Konstantin <55506460+liubinski27@users.noreply.github.com>
2019-11-15 23:37:58 +0300

    carousel adaptive test

--- src/js/base.js
+++ src/js/base.js
@@ -1,3 +1,20 @@
+function carouselHeight() {
+    var carouselImg = document.querySelector('.carousel');
+    var carouselImgWidth = carouselImg.offsetWidth;
+    var carouselImgHeight;
+    switch(carouselImgWidth) {
+        case carouselImgWidth > 1199: carouselImgHeight = 800;
+        case carouselImgWidth < 1200 && carouselImgWidth > 991: carouselImgHeight = 650;
+        case carouselImgWidth < 992 && carouselImgWidth > 767: carouselImgHeight = 514;
+        case carouselImgWidth < 768 && carouselImgWidth > 575: carouselImgHeight = 395;
+        case carouselImgWidth < 576 && carouselImgWidth > 479: carouselImgHeight = 329;
+        case carouselImgWidth < 480: carouselImgHeight = 219;
+    } 
+    document.querySelector('.carousel').style.height = carouselImgHeight + 'px';
+}
+
+carouselHeight();
+
 var buttonS = document.getElementById('menu__btn');
 
 var menu = document.getElementById('hamburger-menu');
@@ -39,4 +56,12 @@ function changeImg(value, ifLastValue) {
     else {
         imgs[value + 1].classList.add('active');
     }
+    carouselHeight();
 }
+
+/*function carouselHeight() {
+    var carouselImg = document.querySelector('.carousel');
+    var carouselImgWidth = carouselImg.offsetWidth;
+    var carouselImgHeight = carouselImgWidth / 1.46;
+    document.querySelector('.carousel').style.height = carouselImgHeight + 'px';
+}*/
