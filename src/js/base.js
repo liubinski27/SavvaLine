5b7e19faaeea6678ead912a84130e6ae79e2c147
Konstantin <55506460+liubinski27@users.noreply.github.com>
2019-11-16 03:13:29 +0300

    carousel btns test

--- src/js/base.js
+++ src/js/base.js
@@ -2,14 +2,14 @@ function carouselHeight() {
     var carouselImg = document.querySelector('.carousel');
     var carouselImgWidth = carouselImg.offsetWidth;
     var carouselImgHeight;
-    switch(carouselImgWidth) {
+    switch (carouselImgWidth) {
         case carouselImgWidth > 1199: carouselImgHeight = 800;
         case carouselImgWidth < 1200 && carouselImgWidth > 991: carouselImgHeight = 650;
         case carouselImgWidth < 992 && carouselImgWidth > 767: carouselImgHeight = 514;
         case carouselImgWidth < 768 && carouselImgWidth > 575: carouselImgHeight = 395;
         case carouselImgWidth < 576 && carouselImgWidth > 479: carouselImgHeight = 329;
         case carouselImgWidth < 480: carouselImgHeight = 219;
-    } 
+    }
     document.querySelector('.carousel').style.height = carouselImgHeight + 'px';
 }
 
@@ -30,11 +30,11 @@ function myFun() {
     }
 }
 
-var imgs = document.querySelectorAll('div.carousel > img');
+setInterval(checkNowForNext, 4000);
 
-setInterval(checkNow, 5000);
+var imgs = document.querySelectorAll('div.pics > img');
 
-function checkNow() {
+function checkNowForNext() {
     for (var i = 0; i < imgs.length; i++) {
         var now = imgs[i].classList;
         var ifLast = false;
@@ -42,21 +42,65 @@ function checkNow() {
             if (i === imgs.length - 1) {
                 var ifLast = true;
             }
-            changeImg(i, ifLast);
+            carouselNext(i, ifLast);
             break;
         }
     }
 }
 
-function changeImg(value, ifLastValue) {
-    imgs[value].classList.remove('active');
-    if (ifLastValue) {
-        imgs[0].classList.add('active');
+function carouselNext(value, ifLastValue) {
+    imgs[value].classList.add('opacityHidden');
+    setTimeout(function () {
+        imgs[value].classList.remove('active');
+        if (ifLastValue) {
+            imgs[0].classList.add('active');
+            setTimeout(function() {
+                imgs[0].classList.remove('opacityHidden');
+            }, 100);
+        }
+        else {
+            imgs[value + 1].classList.add('active');
+            setTimeout(function() {
+                imgs[value + 1].classList.remove('opacityHidden');
+            }, 100);
+        }
+        carouselHeight();
+    }, 1000);
+    setTimeout(checkNowForNext, 4000);
+}
+
+function checkNowForPrev() {
+    for (var i = 0; i < imgs.length; i++) {
+        var now = imgs[i].classList;
+        var ifFirst = false;
+        if (now == 'active') {
+            if (i === 0) {
+                var ifFirst = true;
+            }
+            carouselPrev(i, ifFirst);
+            break;
+        }
     }
-    else {
-        imgs[value + 1].classList.add('active');
-    }
-    carouselHeight();
+}
+
+function carouselPrev(value, ifFirstValue) {
+    imgs[value].classList.add('opacityHidden');
+    setTimeout(function () {
+        imgs[value].classList.remove('active');
+        if (ifFirstValue) {
+            imgs[imgs.length - 1].classList.add('active');
+            setTimeout(function() {
+                imgs[imgs.length - 1].classList.remove('opacityHidden');
+            }, 100);
+        }
+        else {
+            imgs[value - 1].classList.add('active');
+            setTimeout(function() {
+                imgs[value - 1].classList.remove('opacityHidden');
+            }, 100);
+        }
+        carouselHeight();
+    }, 1000);
 }
 
 /*function carouselHeight() {
@@ -65,3 +109,9 @@ function changeImg(value, ifLastValue) {
     var carouselImgHeight = carouselImgWidth / 1.46;
     document.querySelector('.carousel').style.height = carouselImgHeight + 'px';
 }*/
+
+var btnLeft = document.querySelector('.btnLeft');
+btnLeft.addEventListener('click', checkNowForPrev);
+
+var btnRight = document.getElementById('btnRight');
+btnRight.addEventListener('click', checkNowForNext);
